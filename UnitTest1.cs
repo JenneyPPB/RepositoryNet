f930767a86d82d6af3296e1c44c236c38c39dd6e
JenneyPPB <40428367+JenneyPPB@users.noreply.github.com>
2018-06-27 01:15:27 -0700

    Commit UnitTests de App Jsonnet
    Test unitario de Jsonnet

---
+++ UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Json.Integration.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
